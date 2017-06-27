05b41eb693987114c8cf067f7cb4d9f4e5961365
vijay vikram singh <vsingh@axway.com>
2017-06-27 14:56:38 +0800

    app.js updated

--- example/app.js
+++ example/app.js
@@ -1,20 +1,77 @@
 
 
-var Camera = require('ti.cameraview');
+var TiCamera = require('ti.cameraview');
 var win = Titanium.UI.createWindow();
 var btn = Ti.UI.createButton({
   title: 'OPEN CAMERA'
 });
+var camera;
 btn.addEventListener('click',function (){
-   Camera.openCamera({
+
+ camera = TiCamera.createCamera({
                saveToPhotoGallery:true,
-               torchMode:Camera.TORCH_MODE_ON,
-                     flashMode:Camera.FLASH_MODE_ON,
-                     success: function(event)
-                     {
-                        win.backgroundImage = event.media;
-                     }
-                     });
+               torchMode:TiCamera.TORCH_MODE_OFF,
+               flashMode:TiCamera.FLASH_MODE_ON,
+               cameraType:TiCamera.CAMERA_TYPE_REAR,
+               showControl:false,
+
+                success: function(event)
+                 {
+                    win.backgroundImage = event.media;
+                  }
+                });
+                camera.add(changeCamera);
+                camera.add(torch);
+                camera.add(capture);
+
+                camera.open();
+              });
+
+var changeCamera = Ti.UI.createButton({
+                                      right: 40,
+                                      top: 50,
+                                      title:'Change Camera',
+                                      backgroundColor:'red'
+                                      });
+changeCamera.addEventListener('click', function(e){
+                              if(camera.cameraType == TiCamera.CAMERA_TYPE_REAR) {
+                              camera.setCameraType(TiCamera.CAMERA_TYPE_FRONT);
+                              }
+                              else
+                              {
+                              camera.setCameraType(TiCamera.CAMERA_TYPE_REAR);
+                              }
+                              });
+var torch = Ti.UI.createButton({
+                                     left: 40,
+                                     top: 50,
+                                     title:'Torch Mode',
+                                     backgroundColor:'red'
+                                     });
+torch.addEventListener('click', function(e){
+                             Ti.API.info(camera.torchMode);
+                             Ti.API.info(TiCamera.TORCH_MODE_OFF);
+                             Ti.API.info(TiCamera.TORCH_MODE_ON);
+                             
+                             if(camera.torchMode == TiCamera.TORCH_MODE_OFF) {
+                             Ti.API.info('to on');
+                             camera.setTorchMode(TiCamera.TORCH_MODE_ON);
+                             }
+                             else {
+                             Ti.API.info('to off');
+                             camera.setTorchMode(TiCamera.TORCH_MODE_OFF);
+                             }
+                           });
+
+var capture = Ti.UI.createButton({
+                                 bottom : 40,
+                                 title: 'Capture',
+                                 backgroundColor: 'red'
+});
+
+capture.addEventListener('click', function(e){
+
+camera.captureImage();
 });
 win.add(btn);
 win.open();
