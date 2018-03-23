d482c31edd77b1f95f7677c014435c9a6a26d72b
vijay vikram singh <vsingh@axway.com>
2018-03-23 15:42:16 +0800

    MOve camera creation code in CameraviewModule. In proxy callbacks not work properly.

--- ios/Classes/TiCameraviewModule.h
+++ ios/Classes/TiCameraviewModule.h
@@ -11,7 +11,97 @@
 #import "TiModule.h"
 #import "TiViewProxy.h"
 
-@interface TiCameraviewModule : TiModule {
+@interface TiCameraviewModule : TiModule <TiImageCaptureDelegate> {
+  TiCameraViewController *_cameraViewController;
+  KrollCallback *_successCallback;
+  KrollCallback *_errorCallback;
+  KrollCallback *_cancelCallback;
+  BOOL _saveToPhotoGallery;
 }
+
+/**
+ @brief To be documented.
+ */
+- (void)open:(id)args;
+
+/**
+ @brief To be documented.
+ */
+- (void)close:(__unused id)unused;
+
+/**
+ @brief To be documented.
+ */
+- (void)captureImage:(id)args;
+
+/**
+ @brief To be documented.
+ */
+- (void)add:(id)args;
+
+/**
+ @brief To be documented.
+ */
+- (void)setShowNativeControl:(NSNumber *)showNativeControl;
+
+/**
+ @brief To be documented.
+ */
+- (void)setTorchMode:(NSNumber *)torchMode;
+
+/**
+ @brief To be documented.
+ */
+- (NSNumber *)torchMode;
+
+/**
+ @brief To be documented.
+ */
+- (void)setFlashMode:(NSNumber *)flashMode;
+
+/**
+ @brief To be documented.
+ */
+- (NSNumber *)flashMode;
+
+/**
+ @brief To be documented.
+ */
+- (void)setFocusMode:(NSNumber *)focusMode;
+
+/**
+ @brief To be documented.
+ */
+- (NSNumber *)focusMode;
+
+/**
+ @brief To be documented.
+ */
+- (void)setExposureMode:(NSNumber *)exposureMode;
+
+/**
+ @brief To be documented.
+ */
+- (NSNumber *)exposureMode;
+
+/**
+ @brief To be documented.
+ */
+- (void)setCameraType:(NSNumber *)cameraType;
+
+/**
+ @brief To be documented.
+ */
+- (NSNumber *)cameraType;
+
+/**
+ @brief To be documented.
+ */
+- (void)setSaveToPhotoGallery:(NSNumber *)saveToPhotoGallery;
+
+/**
+ @brief To be documented.
+ */
+- (NSNumber *)saveToPhotoGallery;
 
 @end
